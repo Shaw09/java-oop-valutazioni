0d3028d168a5255df55d4195b9a7300d8d071dda
Shaw <michele_aliffi@yahoo.it>
2021-11-24 16:52:16 +0100

    Aggiunti i file

---
+++ src/it/corso/valutazioni/CalcolaValutazione.java
@@ -0,0 +1,5 @@
+package it.corso.valutazioni;
+
+public class CalcolaValutazione {
+
+}
---
+++ src/it/corso/valutazioni/Main.java
@@ -0,0 +1,10 @@
+package it.corso.valutazioni;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

8cd2cf68061d5f0b17e7b336daf5d925be5bd499
Shaw <michele_aliffi@yahoo.it>
2021-11-25 16:17:11 +0100

    Fine progetto

--- src/it/corso/valutazioni/CalcolaValutazione.java
+++ src/it/corso/valutazioni/CalcolaValutazione.java
@@ -1,5 +1,33 @@
 package it.corso.valutazioni;
 
 public class CalcolaValutazione {
+	
+	int idStundete;
+	int percentualeAssenze;
+	double mediaVoti;
+	
+	CalcolaValutazione(int idStundete, int percentualeAssenze, double mediaVoti) {
+		
+		this.idStundete = idStundete;
+		this.percentualeAssenze = percentualeAssenze;
+		this.mediaVoti = mediaVoti;
+	}
 
+	boolean promosso() {
+		boolean promosso;
+		
+		if(percentualeAssenze > 50) {
+			promosso = false;
+		} else if(percentualeAssenze > 25 && percentualeAssenze <= 50) {
+			promosso = mediaVoti > 2.0;
+		} else {
+			promosso = mediaVoti >= 2.0;
+		}
+		
+		return promosso;
+	
+	}
+	
+	
+	
 }
--- src/it/corso/valutazioni/Main.java
+++ src/it/corso/valutazioni/Main.java
@@ -1,10 +1,41 @@
 package it.corso.valutazioni;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		CalcolaValutazione[] cv = new CalcolaValutazione[20];
+		
+		Random randomGen = new Random();
+		
+		int id;
+		int percentualeAssenze;
+		double mediaVoti;
+		
+		int promossi = 0;
+		
+		for(int i = 0; i < cv.length; i++) {
+			id = i + 1;
+			percentualeAssenze = randomGen.nextInt(101);
+			mediaVoti = randomGen.nextDouble() * 5.0;
+			
+			
+			cv[i] = new CalcolaValutazione(id, percentualeAssenze, mediaVoti);
+			System.out.println("Id studente:  " + cv[i].idStundete);
+			System.out.println("Assenze: " + cv[i].percentualeAssenze);
+			System.out.println("Media voti: " + cv[i].mediaVoti);
+			
+			if(cv[i].promosso()) {
+				System.out.println("Promosso");
+				promossi++;
+			} else {
+				System.out.println("Bocciato");
+			}
+		}
 
+		System.out.println("Promossi " + promossi + " studenti");
+		
 	}
 
 }
